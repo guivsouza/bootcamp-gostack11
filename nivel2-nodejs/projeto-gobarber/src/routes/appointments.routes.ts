96fc8406adf58c29e2da7a9da38f9fc528d325f8
GUILHERME VICENTE DE SOUZA <U04329@pioxii.com.br>
2020-04-24 15:12:09 -0300

    Initial backend app completed

---
+++ nivel2-nodejs/projeto-gobarber/src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express'
+import { parseISO } from 'date-fns'
+
+import AppointmentRepository from '../repositories/AppointmentsRepository'
+import CreateAppointmentService from '../services/CreateAppointmentService'
+
+const appointmentsRouter = Router()
+
+const appointmentRepository = new AppointmentRepository()
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentRepository.all()
+
+  return response.json(appointments)
+})
+
+appointmentsRouter.post('/', (request, response) => {
+  try {
+    const { provider, date } = request.body
+
+    const parsedDate = parseISO(date)
+
+    const createAppointment = new CreateAppointmentService(
+      appointmentRepository,
+    )
+
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate,
+    })
+
+    return response.json(appointment)
+  } catch (err) {
+    return response.status(400).json({ message: err.message })
+  }
+})
+
+export default appointmentsRouter
