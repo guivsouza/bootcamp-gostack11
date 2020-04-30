5fe2c0e919ab3942ec4cb1da95c212a8efb6bb1e
GUILHERME VICENTE DE SOUZA <U04329@pioxii.com.br>
2020-04-30 17:04:20 -0300

    Database gobarber created

---
+++ nivel2-nodejs/backend-gobarber/src/services/CreateAppointmentService.ts
@@ -0,0 +1,32 @@
+import { startOfHour } from 'date-fns'
+import { getCustomRepository } from 'typeorm'
+import Appointment from '../models/Appointment'
+import AppointmentRepository from '../repositories/AppointmentsRepository'
+
+interface Request {
+  provider: string
+  date: Date
+}
+
+class CreateAppointmentService {
+  public async execute({ provider, date }: Request): Promise<Appointment> {
+    const appointmentRepository = getCustomRepository(AppointmentRepository)
+
+    const appointmentDate = startOfHour(date)
+
+    if (await appointmentRepository.findByDate(appointmentDate)) {
+      throw Error('This appointment is already booked')
+    }
+
+    const appointment = appointmentRepository.create({
+      provider,
+      date: appointmentDate,
+    })
+
+    await appointmentRepository.save(appointment)
+
+    return appointment
+  }
+}
+
+export default CreateAppointmentService
