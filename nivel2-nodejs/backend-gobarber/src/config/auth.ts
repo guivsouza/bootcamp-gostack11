9f81394af91d0abbfb088276f3fc3185df357d35
GUILHERME VICENTE DE SOUZA <U04329@pioxii.com.br>
2020-05-05 12:10:46 -0300

    Initial Backend Completed

---
+++ nivel2-nodejs/backend-gobarber/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'aabbccddeeffgghh1122334455',
+    expiresIn: '1d',
+  },
+}
--- nivel2-nodejs/backend-gobarber/src/services/CreateAppointmentService.ts
+++ nivel2-nodejs/backend-gobarber/src/services/CreateAppointmentService.ts
@@ -2,24 +2,25 @@ import { startOfHour } from 'date-fns'
 import { getCustomRepository } from 'typeorm'
 import Appointment from '../models/Appointment'
 import AppointmentRepository from '../repositories/AppointmentsRepository'
+import AppError from '../errors/AppError'
 
 interface Request {
-  provider: string
+  provider_id: string
   date: Date
 }
 
 class CreateAppointmentService {
-  public async execute({ provider, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentRepository = getCustomRepository(AppointmentRepository)
 
     const appointmentDate = startOfHour(date)
 
     if (await appointmentRepository.findByDate(appointmentDate)) {
-      throw Error('This appointment is already booked')
+      throw new AppError('This appointment is already booked')
     }
 
     const appointment = appointmentRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     })
 
