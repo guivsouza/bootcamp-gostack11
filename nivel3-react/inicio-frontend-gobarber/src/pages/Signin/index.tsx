2ce31004afc151c501ae131303c6ebfd4aa67275
GUILHERME VICENTE DE SOUZA <U04329@pioxii.com.br>
2020-05-14 15:47:02 -0300

    Authentication api completed

--- nivel3-react/inicio-frontend-gobarber/src/pages/Signin/index.tsx
+++ nivel3-react/inicio-frontend-gobarber/src/pages/Signin/index.tsx
@@ -14,29 +14,45 @@ import Button from '../../components/Button'
 
 import { Container, Content, Background } from './styles'
 
+import { useAuth } from '../../context/AuthContext'
 import getValidationErrors from '../../utils/getValidationErrors'
+
+interface SignInFormData {
+  email: string
+  password: string
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({})
+  const { signIn } = useAuth()
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('Email obrigatorio')
-          .email('Digite um email válido'),
-        password: Yup.string().required('Senha obrigatória'),
-      })
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({})
 
-      await schema.validate(data, { abortEarly: false })
-    } catch (err) {
-      const errors = getValidationErrors(err)
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('Email obrigatorio')
+            .email('Digite um email válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        })
 
-      formRef.current?.setErrors(errors)
-    }
-  }, [])
+        await schema.validate(data, { abortEarly: false })
+
+        signIn({
+          email: data.email,
+          password: data.password,
+        })
+      } catch (err) {
+        const errors = getValidationErrors(err)
+
+        formRef.current?.setErrors(errors)
+      }
+    },
+    [signIn],
+  )
 
   return (
     <Container>
